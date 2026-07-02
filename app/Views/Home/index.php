c111ff2593d5dfe44596928cec4b1035728458b3
Jozka Siregar <85859565+BatJoz21@users.noreply.github.com>
2026-07-02 10:16:36 +0700

    Initial commit

---
+++ app/Views/Home/index.php
@@ -0,0 +1,9 @@
+<?= $this->extend("Layout/layout") ?>
+
+<?= $this->section("title") ?>Home<?= $this->endSection() ?>
+
+<?= $this->section("main") ?>
+
+    <h1>Hello!</h1>
+
+<?= $this->endSection() ?>
